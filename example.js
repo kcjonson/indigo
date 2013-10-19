9d26749073688b77ef64816eaf7e621d6ac155b7
Kevin Jonson <kcjonson@gmail.com>
2013-10-19 13:56:37 -0700

    Starting examples.js Adding .gitignore

---
+++ example.js
@@ -0,0 +1,10 @@
+indigo = require('./lib/indigo.js');
+
+indigo.connectServer({
+	host: 'server.local',
+	port: 8176,
+	serverPath: '/',
+	variablesPath: 'variables/'
+});
+
+indigo.setVariable('isAwayd', false);
